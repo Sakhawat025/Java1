cb255cd9c526b84528747fed09e107f2e027d5aa
Sakhawat Hossain <141615599+Sakhawat025@users.noreply.github.com>
2024-01-01 17:05:19 +0600

    Add files via upload

---
+++ InputOutput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class InputOutput {
+    public static void main(String args[]){
+        Scanner Sc = new Scanner(System.in);
+        int a=Sc.nextInt();
+        int b=Sc.nextInt();
+        int sum=a+b;
+        System.out.println("The total is "+sum);
+    }
+    
+}

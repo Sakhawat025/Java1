313ab46218dbe2d97f4b87541f649f118fb019f6
Sakhawat Hossain <141615599+Sakhawat025@users.noreply.github.com>
2024-01-01 17:04:59 +0600

    Add files via upload

---
+++ First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String args[]){
+        System.out.println("HeY There Earth");
+        System.out.print("Hey Sakhawat Hossain\n");
+        System.out.print("Hey My all Friend");
+    }
+    
+}

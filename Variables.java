f3e84d622f80c8390d192a0c0321398b79f9dbd0
Sakhawat Hossain <141615599+Sakhawat025@users.noreply.github.com>
2024-01-02 17:54:05 +0600

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String args[]){
+        // Variables
+        int a=10,b=25;
+        int sum = a+b;
+        //System.out.println(sum);
+        int diff=a-b;
+        System.out.println(diff);
+
+    }
+    
+}
+  
